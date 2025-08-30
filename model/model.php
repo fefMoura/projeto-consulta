ce7f6552be65293a50909a764e5d24370255397f
Felipe Ferreira de Moura <fmoura.dev@gmail.com>
2025-08-30 14:39:03 -0300

    1 versão formulario

---
+++ model/model.php
@@ -0,0 +1,30 @@
+<?php
+
+class User
+{
+    public $id;
+    public $nome;
+    public $sobrenome;
+    public $email;
+    public $senha;
+
+    public function __construct($id = null, $nome = '',$sobrenome = '', $email = '', $senha = '')
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
+        $this->email = $email;
+        $this->senha = $senha;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id'    => $this->id,
+            'nome'  => $this->nome,
+            'sobrenome'=> $this->sobrenome,
+            'email' => $this->email,
+            'senha'=> $this->senha
+        ];
+    }
+}
---
+++ view/form01.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="cadastro.php" method="post" autocomplete="on">
+
+    <fieldset>
+        <legend>Cadastro</legend>
+
+        <p>
+            <label for="inome">Nome:</label>
+            <input type="text" name="" id="">
+        </p>
+
+        <p>
+            <label for="isobrenome">Sobrenome:</label>
+            <input type="text" name="sobrenome" id="isobrenome">
+        </p>
+
+        <p>
+            <label for="iiemail">Email:</label>
+            <input type="email" name="email" id="iemail">
+        </p>
+
+        <p>
+            <label for="iisenha">Senha:</label>
+            <input type="password" name="senha" id="isenha">
+        </p>
+
+
+    </fieldset>
+
+    </form>
+</body>
+</html>
